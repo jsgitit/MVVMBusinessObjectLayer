bc4ef2e4cd196898b35a920dce82487b56ca4a17
Jon <jsmartware@aol.com>
2021-03-04 21:21:07 -0800

    Added NotifyStateChanged() event to notify ViewModel of updated price

--- BusinessModels/ProductModel.cs
+++ BusinessModels/ProductModel.cs
@@ -8,7 +8,23 @@ namespace MVVMBusinessObjectLayer.BusinessModels
 {
     public class ProductModel : Product
     {
+        public event Action StateChanged; // event delegate
+        private void NotifyStateChanged() => StateChanged?.Invoke(); // call StateChanged event when needed, if it's not null.
 
+        // Override Price property so we can raise the StateChanged event, after it has been set
+        public new decimal Price
+        {
+            get
+            {
+                return base.Price;
+            }
+            set
+            {
+                // Wenever Price is updated in the Business logic, notify the ViewModel with this event
+                base.Price = value;
+                NotifyStateChanged();
+            }
+        }
     }
 
 
