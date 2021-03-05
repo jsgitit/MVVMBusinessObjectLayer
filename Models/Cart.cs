a1c5a6f8250907d71ab2e94908ff187e947342c9
Jon <jsmartware@aol.com>
2021-03-04 18:45:51 -0800

    Added 4 models

---
+++ Models/Cart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MVVMBusinessObjectLayer.Models
+{
+    public class Cart
+    {
+        public int Id{ get; set; }
+        public int CustomerID { get; set; }
+        public List<CartItem> Items { get; set; } = new()
+    }
+}
